79dcc4908ddacd3fdcfa6d5bb6eaa4ee42bf18b4
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2019-08-18 23:08:42 +0900

    Update Elevator.java

--- src/main/java/net/comorevi/nukkitplugin/Elevator.java
+++ src/main/java/net/comorevi/nukkitplugin/Elevator.java
@@ -9,13 +9,18 @@ import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.network.protocol.PlayerActionPacket;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+import java.io.File;
+import java.util.LinkedHashMap;
 
 public class Elevator extends PluginBase implements Listener {
 
-    public static int elevatorBlock = Block.RED_SANDSTONE_SLAB;
+    public static int elevatorBlock;
 
     @Override
     public void onEnable() {
+        initialize();
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -54,6 +59,26 @@ public class Elevator extends PluginBase implements Listener {
 
                 default:
                     return 1;
+        }
+    }
+
+    public void initialize() {
+        if (!this.getDataFolder().exists()) getDataFolder().mkdirs();
+        Config config = new Config(
+                new File(this.getDataFolder(), "config.yml"),
+                Config.YAML,
+                new LinkedHashMap<String, Object>() {
+                    {
+                        put("ElevatorBlock", Block.RED_SANDSTONE_SLAB);
+                    }
+                });
+        config.save();
+        int id = config.getInt("ElevatorBlock", Block.RED_SANDSTONE_SLAB);
+        try {
+            Block.get(id);
+            elevatorBlock = id;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            elevatorBlock = Block.RED_SANDSTONE_SLAB;
         }
     }
 
